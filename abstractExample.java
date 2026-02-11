be37cae54930bb750009c02cc8ff52d6651a2ef3
bishtebisht <bhoomibisht007@gmail.com>
2026-02-11 21:05:38 +0530

    abstract example

--- abstractExample.java
+++ abstractExample.java
@@ -1,18 +1,48 @@
-abstract class absEg {  //abstract class should be declared with abstract keyword
+// abstract class absEg {  //abstract class should be declared with abstract keyword
 
+//     abstract void start();  //no body, only declaration
+
+//     void fuel(){
+//         System.out.println("fuel");
+//     }
+// }
+
+// class Car extends absEg{
+//     void start(){
+//         System.out.println("car started");
+//     }
+// }
+// class Bike extends absEg{
+//     void start(){
+//         System.out.println("bike started");
+//     }
+// }
+
+// public class abstractExample {
+//     public static void main(String[] args) {
+//         absEg c = new Car();
+//         c.start();
+//         c.fuel();
+
+//         absEg b = new Bike();
+//         b.start();
+//         b.fuel();
+//     }
+// }
+
+abstract class absEg2{
     abstract void start();  //no body, only declaration
 
     void fuel(){
         System.out.println("fuel");
     }
 }
-
-class Car extends absEg{
+class Car2 extends absEg2{
     void start(){
         System.out.println("car started");
     }
 }
-class Bike extends absEg{
+class Bike2 extends absEg2{
     void start(){
         System.out.println("bike started");
     }
@@ -20,11 +50,11 @@ class Bike extends absEg{
 
 public class abstractExample {
     public static void main(String[] args) {
-        absEg c = new Car();
+        absEg2 c = new Car2();
         c.start();
         c.fuel();
 
-        absEg b = new Bike();
+        absEg2 b = new Bike2();
         b.start();
         b.fuel();
     }
